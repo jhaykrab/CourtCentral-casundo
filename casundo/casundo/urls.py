2cac6fc8ff5e9ec13fedb9b2aa267a7d43949df3
jhaykrab <jakecasundo15@gmail.com>
2024-12-18 18:55:57 +0800

    first commmit

---
+++ casundo/casundo/urls.py
@@ -0,0 +1,8 @@
+# casundo/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+]
